92c28b4aefa47b465055e5f58c19a1c7e84ec582
Ibrahim Abdi <ibester123@gmail.com>
2019-03-03 13:40:27 -0500

    medical form update works

--- AndroidProject/app/src/main/java/com/example/profile/Retrofit/INodeJs.java
+++ AndroidProject/app/src/main/java/com/example/profile/Retrofit/INodeJs.java
@@ -1,10 +1,16 @@
 package com.example.profile.Retrofit;
 
+import com.example.profile.Classes.MedicalForm;
+
+import org.json.JSONObject;
+
 import io.reactivex.Observable;
+import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
-
+import java.util.List;
 
 public interface INodeJs {
 
@@ -22,4 +28,7 @@ public interface INodeJs {
                                     @Field("lastName") String lastName,
                                     @Field("password") String password);
 
+    @POST("user/updateInfo")
+    @FormUrlEncoded
+    Call<String> updateMed(@Body String m);
 }
--- AndroidProject/app/src/main/java/com/example/profile/httpRequestHelpers/httpPostRequest.java
+++ AndroidProject/app/src/main/java/com/example/profile/httpRequestHelpers/httpPostRequest.java
@@ -27,6 +27,11 @@ public class httpPostRequest extends AsyncTask<String, Void, Void> {
         }
     }
 
+    public void setJSON(JSONObject s){
+        this.postData = s;
+
+    }
+
     static String convertInputStreamToString(java.io.InputStream is) {
         java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
         return s.hasNext() ? s.next() : "";
