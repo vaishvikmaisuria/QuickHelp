63ba31d72a49cddad7389f5a3353a6859fc765b0
christinetg <christinee.taing@gmail.com>
2019-03-25 23:20:45 -0400

    can locate patient on doctor's map fragment

--- AndroidProjectSprint3/app/src/main/java/com/example/profile/MapFragment.java
+++ AndroidProjectSprint3/app/src/main/java/com/example/profile/MapFragment.java
@@ -20,6 +20,7 @@ import android.view.ViewGroup;
 import com.example.profile.directionhelpers.FetchURL;
 import android.location.LocationListener;
 
+import com.example.profile.httpRequestHelpers.httpPostRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -32,6 +33,13 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
 
 public class MapFragment extends Fragment implements OnMapReadyCallback, LocationListener {
     GoogleMap mGoogleMap;
@@ -40,13 +48,14 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
     Polyline currentPolyline;
     private LocationManager locationManager;
     private final int REQUEST_FINE_LOCATION = 1234;
+    String endpoint = "https://quick-health.herokuapp.com";
+    private Double patientlat = null;
+    private Double patientlong = null;
 
     private Location onlyOneLocation;
     private MarkerOptions place1, place2;
 
-    public MapFragment() {
-        // Required empty public constructor
-    }
+    public MapFragment() {}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -80,6 +89,8 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        JSONObject json = null;
+        String sosActive = null;
         MapsInitializer.initialize(getContext());
         mGoogleMap = googleMap;
 
@@ -93,35 +104,36 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
         double latitude = location.getLatitude();
         double longitude = location.getLongitude();
 
-//        new LatLng(43.5913458, -79.6461550)
+        sosActive = getSOSId();
+        if (sosActive != "null") {
+            json = getSOSInfo(sosActive);
+            try {
+                patientlat = (double) json.get("latitude");
+                patientlong = (double) json.get("longitude");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
 
+            place1 = new MarkerOptions().position(new LatLng(latitude, longitude)).title("You");
 
-//        //long
-//        Double patientlong = sosData.getLocationlong();
-//        //lat
-//        Double patientlat = sosData.getLocationlat();
+            if (patientlong != null && patientlat != null) {
+                place2 = new MarkerOptions().position(new LatLng(patientlat, patientlong)).title("Patient");
+                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                mGoogleMap.setMyLocationEnabled(true);
 
-//        Log.d("latitude", patientlat.toString());
-//        Log.d("longitude", patientlong.toString());
+                new FetchURL(this).execute(getUrl(place1.getPosition(), place2.getPosition(), "driving"), "driving");
 
-        place1 = new MarkerOptions().position(new LatLng(latitude, longitude)).title("You");
+                googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
-        place2 = new MarkerOptions().position(new LatLng(43.591092,-79.6468034)).title("Patient");
-        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
+                googleMap.addMarker(place1);
+                googleMap.addMarker(place2);
+                CameraPosition Liberty = CameraPosition.builder().target(new LatLng(latitude, longitude)).zoom(18).bearing(0).tilt(45).build();
+
+                googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(Liberty));
+            }
         }
-        mGoogleMap.setMyLocationEnabled(true);
-
-        new FetchURL(this).execute(getUrl(place1.getPosition(), place2.getPosition(), "driving"), "driving");
-
-
-        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-
-        googleMap.addMarker(place1);
-        googleMap.addMarker(place2);
-        CameraPosition Liberty = CameraPosition.builder().target(new LatLng(latitude, longitude)).zoom(18).bearing(0).tilt(45).build();
-
-        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(Liberty));
     }
 
     private String getUrl(LatLng origin, LatLng dest, String directionMode) {
@@ -165,22 +177,74 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
     }
 
     @Override
-    public void onLocationChanged(Location location) {
-
-    }
+    public void onLocationChanged(Location location) {}
 
     @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
+    public void onStatusChanged(String provider, int status, Bundle extras) {}
 
     @Override
-    public void onProviderEnabled(String provider) {
-
-    }
+    public void onProviderEnabled(String provider) {}
 
     @Override
-    public void onProviderDisabled(String provider) {
+    public void onProviderDisabled(String provider) {}
 
+
+    /*
+    *   Get current logged in user's active sos id
+    */
+    public String getSOSId() {
+        String sosActive = null;
+
+        try {
+
+            // Get current logged in user's id
+            String id = User.getUser().get("_id").toString();
+
+            // Create request body
+            Map<String, String> body = new HashMap<>();
+            body.put("id", id);
+
+            // Create request to fetch user info
+            httpPostRequest userInfoTask = new httpPostRequest(body);
+
+            String result = userInfoTask.execute( endpoint + "/user/specificUserInfo").get();
+            JSONObject sosInfo = new JSONObject(result.substring(8, result.length()-1));
+            sosActive = sosInfo.get("sosActive").toString();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return sosActive;
+    }
+
+
+    /*
+    *   Get sos info
+    */
+    public JSONObject getSOSInfo(String sos) {
+        JSONObject json = null;
+
+        // Create request body
+        Map<String, String> body = new HashMap<>();
+        body.put("id", sos);
+
+        // Create request to fetch an SOS info
+        httpPostRequest sosInfoTask = new httpPostRequest(body);
+
+        try {
+            String res = sosInfoTask.execute( endpoint + "/sos/specificSOS").get();
+            json = new JSONObject(res.substring(7, res.length()-1));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return json;
     }
 }
+
